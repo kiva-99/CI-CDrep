23860020e3f585a58417c053c929151b02641063
Welizary <welizary@tutamail.com>
2026-03-02 21:29:57 +0300

    Init3

--- app/calculator.py
+++ app/calculator.py
@@ -2,8 +2,8 @@ def add(a,b):
     return a+b
 
 def dev(a, b):
-   if b == 0:
-	raise ValueError("Деление на ноль")
-   return a / b
+    if b == 0:
+        raise ValueError("Деление на ноль")
+    return a / b
 
  
