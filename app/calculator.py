2c62747b52caafdaeb0d7c869c5a65011cce469f
roman <k.ivanovconn@gmail.com>
2026-03-02 22:03:25 +0300

    Update: отправляю актуальные файлы на сервер2

--- app/calculator.py
+++ app/calculator.py
@@ -1,18 +1,8 @@
 def add(a: int, b: int) -> int:
-<<<<<<< HEAD
     return a + b
-
-
-def dev(a: int, b: int) -> float:
-    if b == 0:
-        raise ValueError("Деление на ноль")
-    return a / b
-=======
-    return a + b  
 
 
 def divide(a: int, b: int) -> float:
     if b == 0:
         raise ValueError("Division by zero")
     return a / b
->>>>>>> f1b7249f39dc99c36f8b154ef62a8875863595b1
