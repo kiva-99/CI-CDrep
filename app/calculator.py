538492bfa793e10418510a8647e106fa68bf211d
Welizary <welizary@tutamail.com>
2026-03-02 21:34:39 +0300

    Init4

--- app/calculator.py
+++ app/calculator.py
@@ -1,9 +1,8 @@
-def add(a,b):
-    return a+b
+def add(a, b):
+    return a + b
+
 
 def dev(a, b):
     if b == 0:
         raise ValueError("Деление на ноль")
-    return a / b
-
- 
+    return a / b 
